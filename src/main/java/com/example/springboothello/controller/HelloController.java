3c128a4ec51c089220064265e6623b890b0c88ce
xinxing <2216252906@qq.com>
2022-08-31 14:59:42 +0800

    新建项目

---
+++ src/main/java/com/example/springboothello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboothello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller//RestController返回实体对象，Controller返回页面
+public class HelloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello,SpringBoot!";
+    }
+}
